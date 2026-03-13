404075777f6c665af138544124898e234887bb7e
PEDRO HENRIQUE PONCIANO DA SILVA <ph.ponciano07@gmail.com>
2026-03-13 12:42:26 -0300

    atv3

--- script.js
+++ script.js
@@ -4,14 +4,17 @@ let btnAdicionar = document.querySelector("#btnAdicionar");
 
 let lista = document.querySelector("#lista");
 
-
 btnAdicionar.addEventListener("click", () => {
 
     let texto = itemLista.value;
 
+    if(texto === "") return;
+
     let li = document.createElement("li");
 
     li.innerText = texto;
+
+    li.className = "list-group-item";
 
     lista.appendChild(li);
 
