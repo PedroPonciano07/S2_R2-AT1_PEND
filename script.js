6c9c5749cbc2a035a5b44a82c65985025aaf1b1c
PEDRO HENRIQUE PONCIANO DA SILVA <ph.ponciano07@gmail.com>
2026-03-13 12:32:47 -0300

    atv1

--- script.js
+++ script.js
@@ -4,15 +4,16 @@ let btnMostrar = document.querySelector("#btnMostrar");
 
 let mensagem = document.querySelector("#mensagem");
 
-
 btnMostrar.addEventListener("click", () => {
 
     let nome = nomeUsuario.value;
 
     if(nome === ""){
         mensagem.innerText = "Digite um nome!";
+        mensagem.className = "text-danger mt-4";
     }else{
         mensagem.innerText = `Olá ${nome}, seja bem-vindo!`;
+        mensagem.className = "text-success mt-4";
     }
 
 });
