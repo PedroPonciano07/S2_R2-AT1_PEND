b3193bf1bb67fbe06defc2dddbe260ff4222a789
PEDRO HENRIQUE PONCIANO DA SILVA <ph.ponciano07@gmail.com>
2026-03-13 12:38:53 -0300

    atv2

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ let texto = document.getElementById("texto");
 
 let btnAlterar = document.querySelector("#btnAlterar");
 
-
 btnAlterar.addEventListener("click", () => {
 
     texto.innerText = "Texto alterado com Sucesso!"
